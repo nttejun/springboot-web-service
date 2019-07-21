2be8cee83eb8125ead1048158c54206408e867c0
nttejun <nttejun@gmail.com>
2019-07-21 20:28:14 +0900

    Added spring security

---
+++ src/main/java/spring/boot/webservice/config/Security.java
@@ -0,0 +1,20 @@
+package spring.boot.webservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class Security extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
+
